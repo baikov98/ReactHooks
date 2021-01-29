8631096d1c30c9d0cfd26108261116dbd91be1b6
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 12:21:31 +0500

    useEffect done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from 'react'
 function App() {
   const [type, setType] = useState('users')
   const [data, setData] = useState([])
+  const [pos, setPos] = useState({
+    x: 0, y: 0
+  })
   /* useEffect(() => {
     console.log(11)
   }) */
@@ -13,7 +16,24 @@ function App() {
     fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then(response => response.json())
       .then(json => setData(json))
+      return () => {
+        console.log('clean')
+      }
   }, [type])
+
+  const mouseMoveHandler = e => {
+    setPos({
+      x: e.clientX,
+      y: e.clientY
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseMoveHandler)
+    return () => {
+      window.removeEventListener('mousemove', mouseMoveHandler)
+    }
+  }, [])
 
   return (
     <div>
@@ -21,7 +41,8 @@ function App() {
      <button onClick={() => setType('users')}>Пользователи</button>
      <button onClick={() => setType('todos')}>Todo</button>
      <button onClick={() => setType('posts')}>Посты</button>
-     <pre> {JSON.stringify(data, null, 2)}</pre>
+     {/* <pre> {JSON.stringify(data, null, 2)}</pre> */}
+     <pre>{JSON.stringify(pos, null, 2)}</pre>
     </div>
   )
 }
