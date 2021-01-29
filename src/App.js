ea826e5fd2e5fd26d98886dabd372bf5bccdc535
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 22:52:55 +0500

    final custom hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { useEffect, useState,  } from 'react'
 
 
-function App() {
-  const [name, setName] = useState('')
-  const changeHandler = e => {
-    setName(e.target.value)
+function useLogger(value) {
+  useEffect(() => {
+      console.log('vla changed ', value)
+  }, [value])
+}
+
+function useInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    setValue(event.target.value)
   }
+
+  const clear = () => setValue('')
+
+  return {
+    bind: {value, onChange},
+    value,
+    clear
+  }
+}
+
+function App() {
+  const input = useInput('')
+
+  useLogger(input.value)
+
   return (
     <div className={'container pt-3'}>
       
-      <input type='text' value={name} onChange={changeHandler}></input>
-      <h1>{name}</h1>
+      <input type='text' {...input.bind}></input>
+      <button onClick={input.clear}>O4is</button>
+      <h1>{input.value} </h1>
     </div>
   )
 }
