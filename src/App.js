68bfcfa0b07f5b9e5f1cd61a3dddc04f7d9204c7
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 12:02:23 +0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
+import userEvent from '@testing-library/user-event';
 import React, { useState } from 'react'
 
 
-function computeInitialCounter() {
-  console.log('some')
-  return Math.trunc(Math.random()*20)
-}
-
 function App() {
-  const [counter, setCounter]= useState(() => computeInitialCounter())
-  const [state, setState] = useState({
-    title: 'Счетчик',
-    date: Date.now()
-  })
+  const [type, setType] = useState('users')
 
-
-  function increment() {
-    setCounter((prevCounter) => {
-        return prevCounter + 1
-    })
-
-  }
-  function decrement() {
-    setCounter(counter-1)
-  }
-
-  function updateTitle() {
-    setState(prev => {
-      return {
-        ...prev,
-        title: 'dfasdfdsf'
-      }
-    })
-
-  }
   return (
     <div>
-      <h1>Счетчик {counter}</h1>
-      <button onClick={increment} className='btn btn-success'>Добавить</button>
-      <button onClick={decrement} className='btn btn-danger'>Убрать</button>
-      <button onClick={updateTitle} className='btn btn-default'>Изменить название</button>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+     <h1>Ресурс: {type}</h1>
+     <button onClick={() => setType('users')}>Пользователи</button>
+     <button onClick={() => setType('todo')}>Todo</button>
+     <button onClick={() => setType('posts')}>Посты</button>
     </div>
   )
 }
