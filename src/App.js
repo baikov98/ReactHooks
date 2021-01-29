3b99f6e08c81120bc83e919e70ef33c82eded0e3
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 11:56:00 +0500

    objects setState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import React, { useState } from 'react'
 
+
+function computeInitialCounter() {
+  console.log('some')
+  return Math.trunc(Math.random()*20)
+}
+
 function App() {
-  const [counter, setCounter ]= useState(0)
+  const [counter, setCounter]= useState(() => computeInitialCounter())
+  const [state, setState] = useState({
+    title: 'Счетчик',
+    date: Date.now()
+  })
+
 
   function increment() {
     setCounter((prevCounter) => {
         return prevCounter + 1
     })
-    
+
   }
   function decrement() {
     setCounter(counter-1)
+  }
+
+  function updateTitle() {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'dfasdfdsf'
+      }
+    })
+
   }
   return (
     <div>
       <h1>Счетчик {counter}</h1>
       <button onClick={increment} className='btn btn-success'>Добавить</button>
       <button onClick={decrement} className='btn btn-danger'>Убрать</button>
+      <button onClick={updateTitle} className='btn btn-default'>Изменить название</button>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
     </div>
   )
 }
