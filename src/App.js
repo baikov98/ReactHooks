83e98dfd661eebefaa6971604599bbe938d763a2
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 12:12:30 +0500

    useEffect fetch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import userEvent from '@testing-library/user-event';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 function App() {
   const [type, setType] = useState('users')
+  const [data, setData] = useState([])
+  /* useEffect(() => {
+    console.log(11)
+  }) */
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, [type])
 
   return (
     <div>
      <h1>Ресурс: {type}</h1>
      <button onClick={() => setType('users')}>Пользователи</button>
-     <button onClick={() => setType('todo')}>Todo</button>
+     <button onClick={() => setType('todos')}>Todo</button>
      <button onClick={() => setType('posts')}>Посты</button>
+     <pre> {JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
