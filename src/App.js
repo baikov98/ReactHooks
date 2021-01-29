6e0d965647be67ae10bbf020deb6daea3611ecec
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-29 21:20:53 +0500

    base

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-
 import React, { useEffect, useState,  } from 'react'
 
 
 function App() {
-
-
+  const [name, setName] = useState('')
+  const changeHandler = e => {
+    setName(e.target.value)
+  }
   return (
     <div className={'container pt-3'}>
-
+      
+      <input type='text' value={name} onChange={changeHandler}></input>
+      <h1>{name}</h1>
     </div>
   )
 }
